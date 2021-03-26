248e4273b1ec7e3a0dc673aec6cdcea51160f33b
blizzyblake278 <magicguitar19@gmail.com>
2021-03-26 17:18:37 -0500

    added leaderboard button and fixed orientation

---
+++ app/src/main/java/com/example/dice_roller_app/SecondAct.kt
@@ -0,0 +1,12 @@
+package com.example.dice_roller_app
+
+import androidx.appcompat.app.AppCompatActivity
+
+//class SecondAct: AppCompatActivity() {
+
+
+//    override fun onCreate(savedInstanceState){
+//
+//    }
+//
+//}
